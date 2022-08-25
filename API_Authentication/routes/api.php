60635fe630927d0b1be322489d22e9589cd856e5
MFaisalQasim <77318032+MFaisalQasim@users.noreply.github.com>
2022-08-25 00:52:44 -0700

    Initial Commit

--- API_Authentication/routes/api.php
+++ API_Authentication/routes/api.php
@@ -2,11 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
-|--------------------------------------------------------------------------
+--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
@@ -18,11 +18,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
-Route::group(['middleware' => 'auth:sanctum'], function(){
-    //All secure URL's
-
-    });
-
-
-Route::post("login",[UserController::class,'index']);
+// use Illuminate\Http\Request;
+ 
+// Route::middleware('auth:api')->get('/user', function(Request $request) {
+//     return $request->user();
+// });
